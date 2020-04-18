f8e8fbcfeb30eaa9bdf563861760dec38e697152
Austin LaClair <austinlaclair@austins-mbp.attlocal.net>
2020-04-18 19:00:21 -0400

    finishing touches

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
-const words = ['murder', 'blood', 'knife', 'haunt', 'death']
+const words = ['murder', 'blood', 'knife', 'haunt', 'death', 'ghost', 'evil', 'possesed', 'skeleton', 'zombie', 'slasher', 'demon', 'bones',
+'casket', 'tomb', 'cobweb', 'corpse', 'spider', 'pumpkin', 'poltergeist', 'mummy']
 let wins = 0
 let guessesRemaining = 10
 let guessedLetters
@@ -23,12 +24,15 @@ let underscores = () => {
 
     //********************************************** Main game ************************************************** 
     var letterGuessed = document.addEventListener('keypress', function(event) {
-        if (letterGuessed = answer.includes(event.key.toLowerCase())) {         //if key pressed is correct, it will be added to the string. If not it will be pushed to the wrong guess array
+        document.getElementById('press').innerHTML = ''
+//if key pressed is correct, it will be added to the string. If not it will be pushed to the wrong guess array
+        if (letterGuessed = answer.includes(event.key.toLowerCase())) {
             for (j = 0; j < answer.length; j++) {
                 if (answer[j] === event.key) {
                 underscoreLength[j] = event.key
                 }
             }
+// if the word is complete, the game chooses a new word and increases wins counter by 1
             document.getElementById('word').innerHTML = underscoreLength.join(' ')
             if (underscoreLength.join('').toString() == answer) {
                 wins++
@@ -41,12 +45,13 @@ let underscores = () => {
             document.getElementById('winsValue').innerHTML = wins;
             document.getElementById('word').innerHTML = underscoreLength.join(' ');
         }
+//if key pressed is incorrect and isn't a part of the array already, it will be displayed and guesses remaining will decrease by 1
         else {
             if (wrongGuess.includes(event.key)) []
             else {
-                wrongGuess.push(event.key.toLowerCase())
-                guessesRemaining--; //if key pressed is incorrect and isn't a part of the array already, it will be displayed and guesses remaining will decrease by 1
-                
+                wrongGuess.push(event.key.toUpperCase())
+                guessesRemaining--; 
+// if guess reaches 0, the game resets without increasing wins counter             
                 if (guessesRemaining == 0) {
                 wrongGuess = []
                 guessesRemaining = 10
@@ -57,7 +62,7 @@ let underscores = () => {
                 document.getElementById('word').innerHTML = underscoreLength.join(' ');
             }
         }
-        document.getElementById('guessedLettersValue').innerHTML = wrongGuess;
+        document.getElementById('guessedLettersValue').innerHTML = wrongGuess.join(' ');
         document.getElementById('guessValue').innerHTML = guessesRemaining;
         
     })
